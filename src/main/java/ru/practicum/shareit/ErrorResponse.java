d0149b9b040527cf6f50fd7d40b520c2d3ec9a2e
Ferzulla Ferzullaev <Ferzulla6405@gmail.com>
2023-09-23 17:04:34 +0300

    Отправка проекта на проверку 2.0 - Доработка исключений

--- src/main/java/ru/practicum/shareit/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/ErrorResponse.java
@@ -2,18 +2,26 @@ package ru.practicum.shareit;
 
 public class ErrorResponse {
     private final String error;
-    private final String description;
+    //private final String description;
 
-    public ErrorResponse(String error, String description) {
+   /* public ErrorResponse(String error, String description) {
         this.error = error;
         this.description = description;
+    }
+
+    */
+
+    public ErrorResponse(String error) {
+        this.error = error;
     }
 
     public String getError() {
         return error;
     }
 
-    public String getDescription() {
+   /* public String getDescription() {
         return description;
     }
+
+    */
 }
